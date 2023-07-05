82401d028705a1df8946b9742304415c78f31784
BabySparta <123456789+odin@users.noreply.github.com>
2023-07-05 18:01:48 -0400

    Work on Cart css

--- src/components/bodyComponents/CartBody.js
+++ src/components/bodyComponents/CartBody.js
@@ -1,25 +1,58 @@
 import React from "react";
+import trashCan from "../../assets/trashCan.svg";
+import "../../styles/BodyStyles/CartBody.css";
 
 const CartBody = ({ cart, handleQuantityChange }) => {
   return (
     <div className="cartBody">
+      <div className="cartHeader">
+        <h2 className="cartTitle">Your Cart</h2>
+        <div className="amountOfItems">
+          You currently have{" "}
+          {cart.length === 1 ? cart.length + " item" : cart.length + " items"}
+        </div>
+      </div>
       <div className="cartItems">
         {cart.map((item) => (
           <div className="itemWrap" key={cart.indexOf(item)}>
-            <img src={item.img} alt={item.name} />
+            <div className="itemImgWrap">
+              <img src={item.img} alt={item.name} className="itemImg" />
+            </div>
             <div className="itemInfoWrap">
-              <p className="itemName">{item.name}</p>
-              <p className="itemPrice">{item.price}</p>
-              <p className="itemSize">{item.size}</p>
+              <div className="cartItemHeader">
+                <p className="itemName">{item.name}</p>
+                <img
+                  src={trashCan}
+                  alt="remove from cart"
+                  className="removeFromCart"
+                />
+              </div>
+              <div className="priceWrap">
+                <p className="itemPriceDollar">{item.price.split(".")[0]}</p>
+                <p className="itemPriceCent">{item.price.split(".")[1]}</p>
+              </div>{" "}
+              <p className="itemSize">Size: {item.size}</p>
               <div className="quantityWrap">
-                <button className="incrementQuantity" onClick={(e) => handleQuantityChange(e, item, "increment")}>+</button>
+                <button
+                  className="quantityChange"
+                  onClick={(e) => handleQuantityChange(e, item, "decrement")}
+                >
+                  -
+                </button>
                 <input
                   type="number"
                   className="itemQuantity"
                   value={item.quantity}
-                  onChange={(e) => handleQuantityChange(e, item, "itemQuantity")}
+                  onChange={(e) =>
+                    handleQuantityChange(e, item, "itemQuantity")
+                  }
                 />
-                <button className="decrementQuantity" onClick={(e) => handleQuantityChange(e, item, "decrement")}>-</button>
+                <button
+                  className="quantityChange"
+                  onClick={(e) => handleQuantityChange(e, item, "increment")}
+                >
+                  +
+                </button>
               </div>
             </div>
           </div>
